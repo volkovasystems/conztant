6a25ba2dee7b303ae7849acf7e01973803a3e6e6
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-06 13:23:19 +0800

    Updated server/client test.

--- conztant-test.js
+++ conztant-test.js
@@ -3,6 +3,7 @@ const assert = require( "assert" );
 const conztant = require( "./conztant.js" );
 
 assert.equal( conztant( "hello world" ), false, "should return false" );
+
 assert.equal( conztant( "HELLO_WORLD" ), true, "should return true" );
 
 console.log( "ok" );
