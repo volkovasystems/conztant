cca35f0f6ed9da185fb664466152b0d8cdfb7e82
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-20 20:18:48 +0800

    Updated server/client/bridge test.

--- test.module.js
+++ test.module.js
@@ -1,9 +1,108 @@
+"use strict";
 
-const assert = require( "assert" );
+/*;
+	@test-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2017 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-test-license
+
+	@test-configuration:
+		{
+			"package": "conztant",
+			"path": "conztant/test.js",
+			"file": "test.js",
+			"module": "test",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
+			"repository": "https://github.com/volkovasystems/numric.git"
+		}
+	@end-test-configuration
+
+	@test-documentation:
+
+	@end-test-documentation
+
+	@include:
+		{
+			"assert": "should",
+			"conztant": "conztant"
+		}
+	@end-include
+*/
+
+const assert = require( "should" );
+
+//: @server:
 const conztant = require( "./conztant.js" );
+//: @end-server
 
-assert.equal( conztant( "hello world" ), false, "should return false" );
+//: @client:
+const conztant = require( "./conztant.support.js" );
+//: @end-client
 
-assert.equal( conztant( "HELLO_WORLD" ), true, "should return true" );
+//: @bridge:
+const path = require( "path" );
+//: @end-bridge
 
-console.log( "ok" );
+describe( "conztant", ( ) => {
+	//: @!bridge:
+
+	describe( "`conztant( 'HELLO_WORLD' )`", ( ) => it( "should return true", ( ) => assert.equal( conztant( "HELLO_WORLD" ), true ) ) );
+
+	describe( "`conztant( 'hello world' )`", ( ) => it( "should return false", ( ) => assert.equal( conztant( "hello world" ), false ) ) );
+
+	//: @end-bridge
+
+	//: @bridge:
+	let directory = __dirname;
+	let testBridge = path.resolve( directory, "bridge.html" );
+	let bridgeURL = `file://${ testBridge }`;
+
+	describe( "`conztant( 'HELLO_WORLD' )`", ( ) => {
+
+		it( "should return true", ( ) => {
+			let result = browser.url( bridgeURL ).execute( ( ) => conztant( 'HELLO_WORLD' ) );
+
+			assert.equal( result.value, true );
+		} );
+
+	} );
+
+	describe( "`conztant( 'hello world' )`", ( ) => {
+
+		it( "should return false", ( ) => {
+			let result = browser.url( bridgeURL ).execute( ( ) => conztant( 'hello world' ) );
+
+			assert.equal( result.value, false );
+		} );
+
+	} );
+
+	//: @end-bridge
+
+} );
